a60c0b7a2fb9173d2c391a4b32ec52656f55e845
rootkow <rootkow@users.noreply.github.com>
2016-10-15 16:51:23 -0400

    update

--- src/DungeonDriver.java
+++ src/DungeonDriver.java
@@ -7,12 +7,13 @@ public class DungeonDriver {
 
     public static void main(String[] args) {
         Player BigPapa = new Player("BigPapa");
-
+        System.out.println(BigPapa);
+/*
         Weapon weapon;
 
         int cmd = 0;
 
-        switch (command) {
+        switch (cmd) {
             case 1:
                 weapon = new Dagger();
                 break;
@@ -26,6 +27,7 @@ public class DungeonDriver {
         System.out.println(BigPapa.attack());
         System.out.println("BigPapa dealt " + BigPapa.getDamageDealt() + " damage");
         System.out.println(BigPapa.chuckWeapon());
+*/
     }
 
 }
